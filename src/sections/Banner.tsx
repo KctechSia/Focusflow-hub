52e3f8648a58cc54291b21b78633bb3b102e47fe
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-23 22:43:51 +0100

    add banner section responsive in all devices

--- src/sections/Banner.tsx
+++ src/sections/Banner.tsx
@@ -1,3 +1,12 @@
 export const Banner = () => {
-  return null;
+  return (
+    <section className="py-3 text-center bg-[linear-gradient(to_right,rgb(252,214,255,.7),rgb(41,216,255,.7),rgb(255,253,128,.7),rgb(248,154,191,.7),rgb(252,214,255,.7))] bg-opacity-70">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden md:inline">Introducing a completely redesigned interface – </span>
+          <a href="#" className="underline underline-offset-4">Explore the demo</a>
+        </p>
+      </div>
+    </section>
+  );
 };
