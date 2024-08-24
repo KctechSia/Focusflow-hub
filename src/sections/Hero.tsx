686f073cf665e1501dcd71dc6ad56eaab1202abf
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-24 12:22:56 +0100

    add hero section, responsive in all devices

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,40 @@
+import Image from "next/image";
+
+import ArrowWIcon from "@/assets/icons/arrow-w.svg";
+import cursorImage from "@/assets/images/cursor.png";
+import messageImage from "@/assets/images/message.png";
+
 export const Hero = () => {
-  return null;
+  return (
+    <section className="relative py-[72px] md:py-24 text-white bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] overflow-clip">
+      <div className="absolute h-[375px] w-[750px] md:w-[1536px] md:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#b48cde] bg-[radial-gradient(closest-side,#000_82%,#9560eb)] top-[calc(100%-96px)] md:top-[calc(100%-120px)]"></div>
+      <div className="container relative">
+        <div className="flex items-center justify-center">
+          <a href="#" className="border border-white/30 py-1 px-2 rounded-lg inline-flex gap-3">
+            <span className="bg-[linear-gradient(to_right,#f87aff,#fb93d0,#ffdd99,#c3f0b2,#2fd8fe)] bg-clip-text text-transparent">Version 2.0 is here</span>
+            <span className="inline-flex items-center gap-1">
+              <span>Read More</span>
+              <ArrowWIcon />
+            </span>
+          </a>
+        </div>
+        <div className="flex justify-center mt-8">
+          <div className="inline-flex relative">
+            <h1 className="text-7xl md:text-9xl font-bold tracking-tighter text-center inline-flex">
+              One Task
+              <br /> at a Time
+            </h1>
+            <Image src={cursorImage} alt="Cursor image" className="size-[200px] absolute right-[476px] top-[108px] hidden md:inline" />
+            <Image src={messageImage} alt="Message image" className="size-[200px] absolute left-[498px] top-[56px] hidden md:inline" />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-center text-xl mt-8 max-w-md">Celebrate the joy of accomplishment with an app designed to track progress, motivate your efforts, and celebrate your success.</p>
+        </div>
+        <div className="flex justify-center mt-8">
+          <button className="bg-white text-black py-3 px-5 rounded-lg font-medium">Get for free</button>
+        </div>
+      </div>
+    </section>
+  );
 };
