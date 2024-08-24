7b44df6bdfd94fe9f22d15030c0e7a3e22c08d5b
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-24 17:54:18 +0100

    update footer styles

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -8,12 +8,12 @@ export const Footer = () => {
     <section className="bg-black text-white/60 py-5 border-t border-white/20">
       <div className="container">
         <div className="flex flex-col md:flex-row md:justify-between gap-4">
-          <div className="text-center">&copy; 2024 Your Company, Inc. All rights reserved</div>
+          <div className="text-center select-none">&copy; 2024 Your Company, Inc. All rights reserved</div>
           <ul className="flex justify-center gap-2.5">
-            <li className="hover:text-white transition duration-200"><XSocialIcon /></li>
-            <li className="hover:text-white transition duration-200"><InstaIcon /></li>
-            <li className="hover:text-white transition duration-200"><TiktokIcon /></li>
-            <li className="hover:text-white transition duration-200"><YoutubeIcon /></li>
+            <li className="hover:text-white transition duration-200 cursor-pointer"><XSocialIcon /></li>
+            <li className="hover:text-white transition duration-200 cursor-pointer"><InstaIcon /></li>
+            <li className="hover:text-white transition duration-200 cursor-pointer"><TiktokIcon /></li>
+            <li className="hover:text-white transition duration-200 cursor-pointer"><YoutubeIcon /></li>
           </ul>
         </div>
       </div>
