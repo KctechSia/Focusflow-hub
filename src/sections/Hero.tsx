84bffe0462bd3ad7f4345779e6e49fbd5c22b310
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-24 16:00:53 +0100

    add animations (drag) to the hero images section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,17 @@
+"use client";
+import { useRef } from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import ArrowWIcon from "@/assets/icons/arrow-w.svg";
 import cursorImage from "@/assets/images/cursor.png";
 import messageImage from "@/assets/images/message.png";
 
 export const Hero = () => {
+  const sectionRef = useRef<HTMLElement>(null);
+
   return (
-    <section className="relative py-[72px] md:py-24 text-white bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] overflow-clip">
+    <section ref={sectionRef} className="relative py-[72px] md:py-24 text-white bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] overflow-clip">
       <div className="absolute h-[375px] w-[750px] md:w-[1536px] md:h-[768px] lg:w-[2400px] lg:h-[1200px] rounded-[100%] bg-black left-1/2 -translate-x-1/2 border border-[#b48cde] bg-[radial-gradient(closest-side,#000_82%,#9560eb)] top-[calc(100%-96px)] md:top-[calc(100%-120px)]"></div>
       <div className="container relative">
         <div className="flex items-center justify-center">
@@ -24,8 +29,24 @@ export const Hero = () => {
               One Task
               <br /> at a Time
             </h1>
-            <Image src={cursorImage} alt="Cursor image" className="size-[200px] absolute right-[476px] top-[108px] hidden md:inline" />
-            <Image src={messageImage} alt="Message image" className="size-[200px] absolute left-[498px] top-[56px] hidden md:inline" />
+            <motion.div
+              drag
+              dragConstraints={sectionRef}
+              dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+              whileTap={{ cursor: "grabbing" }}
+              className="size-[200px] absolute right-[476px] top-[108px] hidden md:inline cursor-grab"
+            >
+              <Image src={cursorImage} alt="Cursor image" draggable="false" />
+            </motion.div>
+            <motion.div
+              drag
+              dragConstraints={sectionRef}
+              dragTransition={{ bounceStiffness: 300, bounceDamping: 10 }}
+              whileTap={{ cursor: "grabbing" }}
+              className="size-[200px] absolute left-[498px] top-[56px] hidden md:inline cursor-grab"
+            >
+              <Image src={messageImage} alt="Message image" draggable="false" />
+            </motion.div>
           </div>
         </div>
         <div className="flex justify-center">
