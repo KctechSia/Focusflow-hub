efd3f235f073419e4f525c4a96481dde98acc656
Ricardo Leite <ricardo.jose.sousa.leite@gmail.com>
2024-08-24 17:54:46 +0100

    add animations to call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,13 +1,38 @@
+"use client";
+import { useRef } from "react";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+
 import helixImage from "@/assets/images/helix2.png";
 import emojiStarImage from "@/assets/images/emojistar.png";
-import Image from "next/image";
 
 export const CallToAction = () => {
+  const sectionRef = useRef<HTMLElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end end"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [40, -40]);
+
   return (
-    <section className="bg-black text-white py-[72px] md:py-24 text-center overflow-hidden">
+    <section ref={sectionRef} className="bg-black text-white py-[72px] md:py-24 text-center">
       <div className="container max-w-xl relative">
-        <Image src={helixImage} alt="" className="absolute top-6 left-[calc(100%+36px)]" />
-        <Image src={emojiStarImage} alt="" className="absolute -top-[120px] right-[calc(100%+24px)]" />
+        <motion.div
+          style={{
+            translateY
+          }}
+        >
+          <Image src={helixImage} alt="" className="absolute top-6 left-[calc(100%+36px)]" />
+        </motion.div>
+        <motion.div
+          style={{
+            translateY
+          }}
+        >
+          <Image src={emojiStarImage} alt="" className="absolute -top-[120px] right-[calc(100%+24px)]" />
+        </motion.div>
         <h2 className="text-5xl md:text-6xl font-bold tracking-tighter">Get instant access</h2>
         <p className="text-xl text-white/70 mt-5">Celebrate the joy of accomplishment with an app designed to track your progress and motivate your efforts.</p>
         <form className="mt-10 flex flex-col gap-2.5 max-w-sm mx-auto md:flex-row">
